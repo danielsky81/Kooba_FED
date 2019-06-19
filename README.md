1faddbc924cebf88cc70466857e9bb1badc2f2db
danielsky <karma0rg4u@gmail.com>
2019-06-19 17:07:59 +0100

    Final changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Kooba Frontend Challenge
 
+## Setup
+
+- Install all packages as per `package.json`
+- Run `gulp` in CLI to build `dist` directory
+- Run `gulp watch` to run the server on local port `3000`
+
+The site has been deplyed to [Netlify](https://www.netlify.com/) and can be previewed [here](https://keen-mccarthy-878f9f.netlify.com/)
--- dist/script/script.js
+++ dist/script/script.js
@@ -4,7 +4,15 @@ $(document).ready(function(){
             slidesToShow: 3,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 2000,
+            autoplaySpeed: 5000,
+            arrows: false,
+        });
+    } else {
+        $('.section-slider-container').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
             arrows: false,
         });
     }
